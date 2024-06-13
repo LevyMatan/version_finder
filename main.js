d89680c8a5cf9e5e876afdc266bc659616c0c3ed
Matan Levy <levymatanlevy@gmail.com>
2024-06-13 19:31:54 +0300

    feat: Add error alert placeholder to index.html

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // main.js
-const path = require('path')
-const os = require('os')
-const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
-const VersionFinder = require('./version_finder.js')
-const { dialog } = require('electron')
+const path = require("path");
+const os = require("os");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const VersionFinder = require("./version_finder.js");
+const { dialog } = require("electron");
+const { send } = require("process");
 
 let mainWindow;
 
-const isDevMode = process.env.NODE_ENV === 'development'
-console.log("isDevMode: ", isDevMode)
+const isDevMode = process.env.NODE_ENV === "development";
+console.log("isDevMode: ", isDevMode);
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -18,67 +19,67 @@ function createWindow () {
       nodeIntegration: true,
       contextIsolation: false,
     },
-    icon: path.join(__dirname, 'icon-jinny.icns')
-})
+    icon: path.join(__dirname, "icon-jinny.icns"),
+  });
 
-    // Open the DevTools when the app is started
-  mainWindow.loadFile('index.html')
-  if (isDevMode){
-    mainWindow.webContents.openDevTools()
+  // Open the DevTools when the app is started
+  mainWindow.loadFile("index.html");
+  if (isDevMode) {
+    mainWindow.webContents.openDevTools();
   }
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // Main IPC code
-ipcMain.on('init:repo', (e, options) => {
-    console.log("Got into init:repo")
-    const form = {
-        repositoryPath: options.repoPath,
-    }
-    console.log(form)
-    initRepo({ form })
-})
+ipcMain.on("init:repo", (e, options) => {
+  console.log("Got into init:repo");
+  const form = {
+    repositoryPath: options.repoPath,
+  };
+  console.log(form);
+  initRepo({ form });
+});
 
-ipcMain.on('search:version', (e, options) => {
-    console.log("Got into search:version")
-    console.log("options: ", options)
-    searchVersion(options)
-})
+ipcMain.on("search:version", (e, options) => {
+  console.log("Got into search:version");
+  console.log("options: ", options);
+  searchVersion(options);
+});
 
 async function initRepo({ form }) {
-    try {
-        const versionFinder = new VersionFinder(form.repositoryPath)
-        await versionFinder.init()
-        .then((err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("init done")
-            const branches = versionFinder.getBranches()
-            const submodules = versionFinder.getSubmodules()
-            console.log("from initRepo: branches: ", branches)
-            console.log("from initRepo: submodules: ", submodules)
-            mainWindow.webContents.send('init:done', { branches, submodules })
-        })
-        .catch((err) => {
-            console.log("init error: ", err)
-            mainWindow.webContents.send('init:error:invalid-repo-path', { error: err })
-        })
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    const versionFinder = new VersionFinder(form.repositoryPath);
+    await versionFinder
+      .init()
+      .then(() => {
+        console.log("init done");
+        const branches = versionFinder.getBranches();
+        const submodules = versionFinder.getSubmodules();
+        console.log("from initRepo: branches: ", branches);
+        console.log("from initRepo: submodules: ", submodules);
+        mainWindow.webContents.send("init:done", { branches, submodules });
+      })
+      .catch((err) => {
+        sendError(
+          "init:error:invalid-repo-path",
+          "Invalid repository path",
+          err
+        );
+      });
+  } catch (err) {
+    sendError("init:error:invalid-repo-path", "Invalid repository path", err);
+  }
 }
 
 /**
@@ -90,19 +91,19 @@ async function initRepo({ form }) {
  * @returns
  */
 async function findFirstCommit(versionFinder, form) {
-/**
- * The result structure to be passed should be:
- *  {
- *    isValidFirstCommit: true, // Change to false to test the error message
- *    shortShaFirstCommit: "abc123",
- *    commitMessageFirstCommit: "Initial commit",
- *    isValidVersionCommit: true, // Change to false to test the error message
- *    shortShaVersionCommit: "3498t69784kjsdjkv",
- *    commitMessageVersionCommit: "Version: 1.0.0",
- *    version: "1.0.0"
- *  };
- */
-  // Try and get first commit SHA
+  /**
+   * The result structure to be passed should be:
+   *  {
+   *    isValidFirstCommit: true, // Change to false to test the error message
+   *    shortShaFirstCommit: "abc123",
+   *    commitMessageFirstCommit: "Initial commit",
+   *    isValidVersionCommit: true, // Change to false to test the error message
+   *    shortShaVersionCommit: "3498t69784kjsdjkv",
+   *    commitMessageVersionCommit: "Version: 1.0.0",
+   *    version: "1.0.0"
+   *  };
+   */
+
   const searchResultStructure = {
     isValidFirstCommit: false,
     shortShaFirstCommit: "",
@@ -110,30 +111,37 @@ async function findFirstCommit(versionFinder, form) {
     isValidVersionCommit: false,
     shortShaVersionCommit: "",
     commitMessageVersionCommit: "",
-    version: ""
+    version: "",
   };
-  console.log("Entered findFirstCommit")
+  console.log("Entered findFirstCommit");
   console.log("form: ", form);
   try {
-      const firstCommitstruct = await versionFinder.getFirstCommitSha(form.commitSHA, form.branch, form.submodule);
-      console.log("firstCommitstruct: ", firstCommitstruct);
-      if (firstCommitstruct){
-        searchResultStructure.isValidFirstCommit = true;
-        searchResultStructure.shortShaFirstCommit = firstCommitstruct.hash; // Ensure this matches the property name in firstCommitstruct
-        searchResultStructure.commitMessageFirstCommit = firstCommitstruct.message; // Ensure this matches the property name in firstCommitstruct
-      }
-      else {
-        searchResultStructure.isValidFirstCommit = false;
-      }
-      console.log("searchResultStructure: ", searchResultStructure);
-  }
-  catch (err) {
-      console.error(err);
+    const firstCommitstruct = await versionFinder.getFirstCommitSha(
+      form.commitSHA,
+      form.branch,
+      form.submodule
+    );
+    console.log("firstCommitstruct: ", firstCommitstruct);
+    if (firstCommitstruct) {
+      searchResultStructure.isValidFirstCommit = true;
+      searchResultStructure.shortShaFirstCommit = firstCommitstruct.hash; // Ensure this matches the property name in firstCommitstruct
+      searchResultStructure.commitMessageFirstCommit =
+        firstCommitstruct.message; // Ensure this matches the property name in firstCommitstruct
+    } else {
+      searchResultStructure.isValidFirstCommit = false;
+    }
+    console.log("searchResultStructure: ", searchResultStructure);
+  } catch (err) {
+    sendError("search:error:invalid-commit-sha", "Invalid commit SHA", err);
   }
   // Try and get first version commit
   try {
-    if (searchResultStructure.isValidFirstCommit){
-      const result = await versionFinder.getFirstCommitWithVersion(searchResultStructure.shortShaFirstCommit, form.branch, form.submodule);
+    if (searchResultStructure.isValidFirstCommit) {
+      const result = await versionFinder.getFirstCommitWithVersion(
+        searchResultStructure.shortShaFirstCommit,
+        form.branch,
+        form.submodule
+      );
       console.log("search done");
       console.log("result: ", result);
 
@@ -145,15 +153,13 @@ async function findFirstCommit(versionFinder, form) {
         searchResultStructure.isValidVersionCommit = true;
         searchResultStructure.shortShaVersionCommit = commit_hash;
         searchResultStructure.commitMessageVersionCommit = commit_message;
-        searchResultStructure.version = version
+        searchResultStructure.version = version;
       }
     }
-    mainWindow.webContents.send('search:done', searchResultStructure);
+    mainWindow.webContents.send("search:done", searchResultStructure);
     return searchResultStructure; // Return the result from the function
   } catch (err) {
-      console.log("search error: ", err);
-      mainWindow.webContents.send('search:error:invalid-commit-sha', { error: err });
-      throw err; // Rethrow the error if you want to allow the caller to handle it
+    sendError("search:error:invalid-commit-sha", "Invalid commit SHA", err);
   }
 }
 /**
@@ -166,38 +172,60 @@ async function findFirstCommit(versionFinder, form) {
  * @param {string} options.form.submodule - The submodule.
  */
 async function searchVersion(form) {
-  console.log("form = ", form)
-    try {
-        const versionFinder = new VersionFinder(form.repositoryPath)
-        await versionFinder.init()
-        .then((err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("init done")
-        })
-        .catch((err) => {
-            console.log("init error: ", err)
-            mainWindow.webContents.send('init:error:invalid-repo-path', { error: err })
-        })
-
-        findFirstCommit(versionFinder, form)
-
-    } catch (err) {
-        console.error(err)
-    }
+  console.log("form = ", form);
+  try {
+    const versionFinder = new VersionFinder(form.repositoryPath);
+    await versionFinder
+      .init()
+      .then(() => {
+        console.log("init done");
+        findFirstCommit(versionFinder, form);
+      })
+      .catch((err) => {
+        sendError(
+          "error:init:invalid-repo-path",
+          "Invalid repository path",
+          err
+        );
+      });
+  } catch (err) {
+    sendError("error:search:invalid-repo-path", "Invalid repository path", err);
+  }
 }
 
-ipcMain.on('open:directory', async function() {
-    console.log("Got into open:directory")
-    const result = await dialog.showOpenDialog({
-        properties: ['openDirectory']
-    });
+ipcMain.on("open:directory", async function () {
+  console.log("Got into open:directory");
+  const result = await dialog.showOpenDialog({
+    properties: ["openDirectory"],
+  });
 
-    console.log("Selected directory: ", result)
+  console.log("Selected directory: ", result);
 
-    // Send the selected directory to the renderer process
-    if (!result.canceled && result.filePaths.length > 0) {
-        mainWindow.webContents.send('selected:directory', result.filePaths[0]);
-    }
+  // Send the selected directory to the renderer process
+  if (!result.canceled && result.filePaths.length > 0) {
+    mainWindow.webContents.send("selected:directory", result.filePaths[0]);
+  }
 });
+
+/**
+ * Send error messages to the renderer process
+ * @param {string} channel - The channel to send the message to.
+ * @param {string} message - The error message.
+ * @param {Object} error - The error object.
+ */
+function sendError(channel, message, error) {
+  console.error(channel, message, error);
+  console.error("message: ", error.message);
+  console.error("stack: ", error.stack);
+  // Convert the error object into a plain object including message and stack
+  const errorToSend = {
+    message: error.message,
+    stack: error.stack,
+    // Include any other properties you need
+  };
+  mainWindow.webContents.send("error", {
+    channel,
+    message,
+    error: errorToSend,
+  });
+}

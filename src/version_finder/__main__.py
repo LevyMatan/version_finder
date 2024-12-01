4655005dcde097b6bd83971e464fd82e66d12561
Matan Levy <levymatanlevy@gmail.com>
2024-12-01 23:03:01 +0200

    fix(main): :bug: set default path to cwd, fix cal to find version
    BREAKING CHANGE: When no path argument is given, set the repository path to the current working directory. This now complies with the --help message of the program. Add a call to update_repository with the branch argument before calling the find_first_version_containg_commit.

--- src/version_finder/__main__.py
+++ src/version_finder/__main__.py
@@ -1,3 +1,4 @@
+import os
 from .__cli__ import cli_main
 from .__gui__ import gui_main
 from .__common__ import parse_arguments
@@ -18,9 +19,12 @@ def main():
     elif args.gui:
         gui_main(args)
     else:
+        if args.path is None:
+            args.path = os.getcwd()
         if args.path and args.branch and args.commit:
             vf = VersionFinder(path=args.path)
-            version = vf.find_first_version_containing_commit(args.branch, args.commit, args.submodule)
+            vf.update_repository(args.branch)
+            version = vf.find_first_version_containing_commit(args.commit, args.submodule)
             if version:
                 print(f"The first version which includes commit {args.commit} is {version}")
             else:

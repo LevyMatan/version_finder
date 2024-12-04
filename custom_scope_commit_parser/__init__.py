bccdec842b5b3272317266a07b0910f95d54689d
Matan Levy <levymatanlevy@gmail.com>
2024-12-04 14:08:19 +0200

    ci(debug): custom parsers

--- custom_scope_commit_parser/__init__.py
+++ custom_scope_commit_parser/__init__.py
@@ -1,5 +1,5 @@
 from typing import List
-from .parsers import CustomCommitParserFilterByScopeCore, CustomCommitParserFilterByScopeGui, CustomCommitParserFilterByScopeCli
+from .src.parsers import CustomCommitParserFilterByScopeCore, CustomCommitParserFilterByScopeGui, CustomCommitParserFilterByScopeCli
 
 __all__: List[str] = [
     'CustomCommitParserFilterByScopeCore',

a440d2ab4449d72595c2f067c151c9c4a28d833f
Matan Levy <levymatanlevy@gmail.com>
2024-12-04 14:46:43 +0200

    ci(debug):

--- custom_scope_commit_parser/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from .src.parsers import CustomCommitParserFilterByScopeCore, CustomCommitParserFilterByScopeGui, CustomCommitParserFilterByScopeCli
-
-__all__: List[str] = [
-    'CustomCommitParserFilterByScopeCore',
-    'CustomCommitParserFilterByScopeCli',
-    'CustomCommitParserFilterByScopeGui',
-]
--- custom_scope_commit_parser/setup.py
+++ custom_scope_commit_parser/setup.py
@@ -8,13 +8,13 @@ from setuptools import setup, find_packages
 setup(
     name="custom-scope-commit-parser",
     version="1.0.0",
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    install_requires = [],
     description="Custom Commit Parser: to fillter by scope",
     author="Matan Levy",
     author_email="levymatanlevy@gmail.com",
     url="https://github.com/LevyMatan/version_finder",
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-    include_package_data=True,
     license="MIT",  # License information
     python_requires=">=3.7",
 )
